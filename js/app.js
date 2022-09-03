729275e46820db6c3a8c2dbb78c198c51573c89f
resalatislam5 <resalatislam5@gmail.com>
2022-09-03 17:09:30 +0600

    finishDaily-Dose News

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ const displayCategoriesName = async() =>{
         const li = document.createElement('li');
         li.classList.add('cursor-pointer')
         li.innerHTML = `
-        <button onclick ="displayAllNews('${category_id}')"  type="button" class="text-sm md:text-lg md:px-5 md:py-2.5 md:mr-2 mb-2">${category_name}</button>
+        <button onclick ="displayAllNews('${category_id ? category_id:'NO found'}')"  type="button" class="text-sm md:text-lg md:px-5 md:py-2.5 md:mr-2 mb-2">${category_name ? category_name:'NO found'}</button>
         `
         newsCategory.appendChild(li)
         displaySpiner(true)
